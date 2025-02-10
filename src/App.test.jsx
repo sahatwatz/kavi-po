9ddb8ff67070819ec5834e53bc0b56d7abaf7501
sahatwatz <yoke.21@hotmail.com>
2025-02-11 01:18:38 +0700

    try to add more test case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,14 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import "@testing-library/jest-dom";
 
-test("renders the title", () => {
+test("renders the first title", () => {
   render(<App />);
   const title = screen.getByText(/React \+ Express/i);
+  expect(title).toBeInTheDocument();
+});
+
+test("renders the second title", () => {
+  render(<App />);
+  const title = screen.getByText(/Viteq \+ React/i);
   expect(title).toBeInTheDocument();
 });
